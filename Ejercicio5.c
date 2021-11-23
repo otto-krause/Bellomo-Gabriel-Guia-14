bee4c1ec226acb5197fb6f7c111ea0e6d1213eb3
GabrielBellomo <87550283+GabrielBellomo@users.noreply.github.com>
2021-11-23 15:58:56 -0300

    Update Ejercicio5.c

--- Ejercicio5.c
+++ Ejercicio5.c
@@ -4,50 +4,44 @@
 
 int main()
 {
+    int matriz[101][4]={0}, nuev=0, I=0, J=0, cont=0, Leg=0, Trim=0, prom=0;
 
-    int matriz[101][4]={0}, nuev[3]={0}, I=0, J=0, cont=0, Q=0, cant=0, p=0, prom=0;
-
-    for (I=1;I<=100;I++)
+    for (I=1;I<=10;I++)
     {
         printf("Ingresar las 3 notas del alumno %d:\n", I);
 
         for (J=1;J<=3;J++)
         {
-            printf("Trimestre %d: ", J);
+            printf("Trimestre (%d): ", J);
             scanf("%d", &matriz[I][J]);
         }
         printf("\n");
     }
 
-    system("cls");
-
-    printf("Desea cambiar alguna nota? (Si=1|No=2): ");
+    printf("¿Desea cambiar alguna nota? (Si=1|No=2): ");
     scanf("%d", &cont);
 
-    if (cont==1)
+    while (cont==1)
     {
-
-        printf("Cuantas notas desea cambiar?: ");
-        scanf("%d", &cant);
-
         system("cls");
+        printf("Ingrese el numero de legajo del alumno y el bimestre a cambiar\n");
+        printf("N Legajo: ");
+        scanf("%d", &Leg);
+        printf("Trimestre: ");
+        scanf("%d", &Trim);
 
-        for (Q=1;Q<=cant;Q++)
+        nuev=matriz[Leg][Trim];
+
+        printf("Ingrese la nueva nota: ");
+        scanf("%d", &matriz[Leg][Trim]);
+
+        printf("\nEsta seguro de cambiar la nota %d por la nota %d? (Si=1|No=2):\n", nuev, matriz[Leg][Trim]);
+        scanf("%d", &cont);
+
+        if (cont==1)
         {
-            printf("Ingrese el alumno y el bimestre a cambiar\n");
-            printf("Alumno nro: ");
-            scanf("%d", &nuev[1]);
-            printf("Trimestre nro: ");
-            scanf("%d", &nuev[2]);
-
-            nuev[0]=matriz[nuev[1]][nuev[2]];
-
-            printf("Ingrese la nueva nota: ");
-            scanf("%d", &matriz[nuev[1]][nuev[2]]);
-
             system("cls");
-
-            printf("Esta seguro de cambiar la nota %d por la nota %d? (Si=1|No=2):\n", nuev[0], matriz[nuev[1]][nuev[2]]);
+            printf("Continuar? (1=Si|2=No): ");
             scanf("%d", &cont);
             if (cont==1)
             {
@@ -56,35 +50,34 @@ int main()
             }
             else
             {
-                matriz[nuev[1]][nuev[2]]=nuev[0];
-
-                printf("Continuar? (1=Si|2=No): ");
-                scanf("%d", &cont);
             }
-            system("cls");
         }
-
-        printf("N Legajo\tTrimestre 1\tTrimestre 2\tTrimestre 3\tPromedio\n");
-
-        for (I=1;I<=100;I++)
+        else
         {
-            prom=(matriz[I][1]+matriz[I][2]+matriz[I][3])/3;
+            matriz[Leg][Trim]=nuev;
 
-            printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n\n", I, matriz[I][1], matriz[I][2], matriz[I][3], prom);
+            printf("Continuar? (1=Si|2=No): ");
+            scanf("%d", &cont);
+
+            if (cont==1)
+            {
+                system("cls");
+                continue;
+            }
+            else
+            {
+            }
         }
     }
 
-    else
+    system("cls");
+
+    for (I=1;I<=10;I++)
     {
-        system("cls");
+        prom=(matriz[I][1]+matriz[I][2]+matriz[I][3])/3;
 
         printf("N Legajo\tTrimestre 1\tTrimestre 2\tTrimestre 3\tPromedio\n");
-
-        for (I=1;I<=100;I++)
-        {
-            prom=(matriz[I][1]+matriz[I][2]+matriz[I][3])/3;
-
-            printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n\n", I, matriz[I][1], matriz[I][2], matriz[I][3], prom);
-        }
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n\n", I, matriz[I][1], matriz[I][2], matriz[I][3], prom);
     }
+    return 0;
 }
