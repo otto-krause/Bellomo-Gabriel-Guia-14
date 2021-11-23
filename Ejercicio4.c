c726722e99e62a58af9c84c6aa85e5c9b23fce68
GabrielBellomo <87550283+GabrielBellomo@users.noreply.github.com>
2021-11-23 16:22:16 -0300

    Update Ejercicio4.c

--- Ejercicio4.c
+++ Ejercicio4.c
@@ -1,61 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 
 int main()
 {
+    int matriz[4][13]={0}, sumtot=0, sumlin=0, I=0, J=0;
 
-    int matriz[4][13]={0}, menor=0, I=0, J=0;
-    int suma[4]={0};
+    do{
+        printf("Ingresar el numero del colectivo: ");
+        scanf("%d",&J);
 
-    for (I=0;I<3;I++)
-    {
-        printf("Linea numero %d: \n", I+1);
-        for (J=0;J<12;J++)
-        {
-            printf("Colectivo numero %d: ", J+1);
-            scanf("%d", &matriz[I][J]);
-            suma[0]=suma[0]+matriz[I][J];
-        }
-        printf("\n");
-    }
+        if(J>=1){
+            printf("Ingresar la linea del colectivo: ");
+            scanf("%d",&I);
+
+            printf("Ingresar la cantidad recaudad: ");
+            scanf("%d",&matriz[I][J]);
+
+            system("cls");
+            }
+            else{
+                break;
+            }
+    }while(J>=1);
 
     system("cls");
 
-    printf("Recaudacion total por linea de colectivo: ");
-    printf("\n");
-    for (I=0;I<3;I++)
+    for(I=1;I<=3;I++)
     {
+        sumlin=0;
 
-        for (J=0;J<12;J++)
+        printf("Linea %d\n", I);
+
+        for (J=1;J<=12;J++)
         {
-            suma[I+1]=suma[I+1]+matriz[I][J];
+            printf("Recaudacion deL colectivo (%d): %d \n", J, matriz[I][J]);
+
+            sumtot=sumtot+matriz[I][J];
+            sumlin=sumlin+matriz[I][J];
         }
-        printf("Linea %d\t %d", I+1, suma[I+1]);
-        printf("\n");
+        printf("\nRecaudacion de la linea (%d): %d\n\n", I, sumlin);
     }
-
-    printf("\n");
-    printf("\n");
-
-    printf("Recaudacion total por auto: ");
-    printf("\n");
-    for (I=0;I<3;I++)
-    {
-        printf("Linea %d\t   ", I+1);
-
-        for (J=0;J<12;J++)
-        {
-            printf("%d\t", matriz[I][J]);
-        }
-        printf("\n");
-    }
-
-    printf("\n");
-    printf("\n");
-
-    printf("Recaudacion total: ");
-    printf("\n");
-    printf("Linea total\t %d", suma[0]);
+    printf("\nRecaudacion total: %d\n", sumtot);
 }
+
 
